e41fb8136070fb45d1a29d90c494141d312ec85d
Jonathan Chau <chaujt1@uci.edu>
2025-06-05 03:03:19 -0700

    Created the header of Community Board

---
+++ community_board/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <img src="/playoffs_image.jpg" alt="Header image of NBA team logos and the words playoffs." className="Header-img"/>
+            <h1 className="Header-h1">NBA Playoff Contenders</h1>
+        </div>
+    )
+}
+
+export default Header;
