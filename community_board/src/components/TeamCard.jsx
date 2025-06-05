b20c8ccd896fc92e34da4b35f8f11e8d6b1cd5a1
Jonathan Chau <chaujt1@uci.edu>
2025-06-05 04:19:38 -0700

    Prepared all team cards in JSON file. Finalized style changes.

--- community_board/src/components/TeamCard.jsx
+++ community_board/src/components/TeamCard.jsx
@@ -1,12 +1,13 @@
 
 const TeamCard = (props) => {
     const { teamName, teamLocation, teamImage, teamLink, teamRanking} = props;
+    const isLongName = teamName.length > 20; // Adjust threshold as needed
     
     return (
         <div>
             <img src={teamImage} alt={`An image of the ${teamName}`} className="TeamCard-img"/>
             <div className="TeamCard-info">
-                <h2 className="TeamName-text">{teamName}</h2>
+                <h2 className={`TeamName-text${isLongName ? " TeamName-text--small" : ""}`}>{teamName}</h2>
                 <h3 className="TeamLocation-text">{teamLocation}</h3>
                 <h4 className="TeamRanking-text">{teamRanking}</h4>
                 <a href={teamLink} className="TeamLink-text" target="_blank" rel="noopener noreferrer">
